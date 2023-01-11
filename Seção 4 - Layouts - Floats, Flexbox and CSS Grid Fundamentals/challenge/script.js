735cab2bc1639f3307a41f7d488a5840913c176c
LucasPerrotaRoriz <LucasPR1998@protonmail.com>
2023-01-11 20:05:19 -0300

    Floats, Flexbox and GRID

---
+++ Seção 4 - Layouts - Floats, Flexbox and CSS Grid Fundamentals/challenge/script.js
@@ -0,0 +1,10 @@
+/*
+- box-sizing
+
+- Price and shipping side by side
+- No floats on colors
+- Layout. Image is 250px, and then we want 40px between all elements. So you need to calculate width of the two other ones. If that's too hard, just use a width of 240px
+- You will need clearfix class
+
+This will bevome SOOO much easier in flexbox!
+*/
